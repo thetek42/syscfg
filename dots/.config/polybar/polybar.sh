f9dfe80e9b5494803967b06c711c4643dc15deff
thetek <git@thetek.de>
2024-12-07 18:19:47 +0100

    fix(polybar): only display once lmao

--- dots/.config/polybar/polybar.sh
+++ dots/.config/polybar/polybar.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env sh
 
 killall -q polybar
-polybar bar & disown
 
 for monitor in $(xrandr --query | grep " connected" | cut -d" " -f1); do
 	MONITOR=$monitor polybar --reload bar & disown
