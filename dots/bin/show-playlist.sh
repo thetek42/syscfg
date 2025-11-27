a863522b0eb68202000c8d8fad76effbefe5fedb
thetek <git@thetek.de>
2025-11-27 12:22:22 +0100

    feat(mpd): add script for showing playlist

---
+++ dots/bin/show-playlist.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+set -e
+
+# get list of songs in that playlist
+songs="$(mpc -f "%disc%-%track% %title%" playlist "$1")"
+
+# if the playlist is an album, sort by the track number to play it in order
+if [[ "$1" == "A: "* ]]; then
+	songs="$(echo "$songs" | sort -n)"
+else
+	# for playlists, strip the disc and track number
+	songs="$(echo "$songs" | cut -d ' ' -f 3-)"
+fi
+
+
+echo "$songs"
