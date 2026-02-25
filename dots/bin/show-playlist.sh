37def2efc00370a0b8f561bdcbc61ed1da6982f6
thetek <git@thetek.de>
2026-02-25 16:37:50 +0100

    fix(mpd): sort albums without disc number correctly

--- dots/bin/show-playlist.sh
+++ dots/bin/show-playlist.sh
@@ -3,7 +3,8 @@ set -e
 
 if [[ "$1" == "A: "* ]]; then
 	# if the playlist is an album, sort by the track number
-	songs="$(mpc -f "%disc%-%track% %title%" playlist "$1")"
+	# prepent 0 because some songs have an empty disc number
+	songs="$(mpc -f "0%disc%-%track% %title%" playlist "$1")"
 	songs="$(echo "$songs" | sort -t - -k 1n -k 2n)"
 else
 	# if the playlist is a playlist, show titles and sort alphabetically
